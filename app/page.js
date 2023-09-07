eb3ce3410d87397d32b8685a289ae3a849b8fbe3
Livingstone143 <paramalla2004@gmail.com>
2023-09-07 21:53:28 +0530

    typei

--- app/page.js
+++ app/page.js
@@ -17,16 +17,28 @@ export default function Home() {
 
   return (
     <div className={darkMode ? "dark" : ""}>
-    <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
-    <section className=" min-h-screen ">
        
-       <nav className="py-10 mb-12 flex justify-between dark:text-white">
-       <h1 className="text-2xl font-burtons font-extrabold ">Param</h1>
-        <ul className="flex justify-between">
-          <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl"/></li>
-          <li><a className="text-white  bg-gradient-to-r from-cyan-500 to-pink-500 rounded-xl p-2 ml-4 " href="#">Resume</a></li>
-        </ul>
-       </nav>
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
        <div className="text-center p-8 py-8">
        
         <h2 className="text-5xl py-2 text-teal-700 font-medium">Param Jyothi</h2>
